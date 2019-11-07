1381e3cedee45fa55ad5406f6bb15086439f9578
CianDaly <cian.daly117@gmail.com>
2019-11-07 23:11:01 +0000

    NewsItem component complete

--- stories/index.js
+++ stories/index.js
@@ -5,3 +5,20 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import NewsForm from '../src/components/newsForm/';
 
 storiesOf("Hacker App/News Form", module).add("default", () => <NewsForm/>);
+
+import NewsItem from '../src/components/newsItem';
+
+const post = {
+    id: 1 ,
+    title : 'Post 1.',
+    link : 'http://www.foo.bar',
+    author : 'Joe Bloggs',
+    comments : [],
+    upvotes: 10
+};
+
+storiesOf('Hacker App/News Item', module)
+    .add('default', () => <NewsItem post= { post }  />
+    )
+    .add('No hyperlink', () => <NewsItem post= { { ...post, link: ''} }  />
+    )
