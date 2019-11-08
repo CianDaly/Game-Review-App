9992ed6155806ce806a2702a5c216df08670719b
Cian Daly <>
2019-11-08 15:31:54 +0000

    Added stub API

--- src/components/newsList/index.js
+++ src/components/newsList/index.js
@@ -1,4 +1,4 @@
-import React, {Component } from 'react';
+import React, {Component, Fragment } from 'react';
 import NewsItem from '../newsItem/';
 
 export default class NewsList extends Component {
