6b6a61d6933a53ed0fa33742d59ab3c93ad81df0
CianDaly <cian.daly117@gmail.com>
2019-12-12 23:53:54 +0000

    started integration of mongo to api.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import NewsList from './components/newsList/';
 import Form from './components/newsForm/';
-import api from './dataStore/stubAPI';
+//import api from './dataStore/stubAPI';
+import * as api from './api';
 
 import _ from 'lodash';
 import "./App.css";
 
 export default class App extends Component {   
 
-    addNewsItem = (title,  publisher, link) => {
+  state = {posts: [{}]};
+
+  componentDidMount() {
+      api.getAll().then(resp => {
+          this.setState({
+              posts: resp.posts
+          });
+      }).catch(console.error);
+  };
+
+    /*addNewsItem = (title,  publisher, link) => {
         api.add(title,  publisher, link);
         this.setState({});
     };
+    */
+
+    addNewsItem = (title, link) => {
+      api.add(title,link)
+      .then(resp => {
+                    const newPost = {"id":resp.id,"title":title,"link":link,"upvotes":0, "comments":[]};
+                    this.setState({posts: this.state.posts.concat([newPost])});
+      })
+    };
     
+    /*
     incrementUpvote = (id) => {
         api.upvote(id);
         this.setState({});
+    };
+    */
+   
+    incrementUpvote = (id) => {
+      api.upvote(id).then(resp=> {
+             var upvotedPost = _.find(this.state.posts, post=>post.id == id);
+             upvotedPost.upvotes++;  
+             this.setState({})
+           }) ;
     };
 
     removeNewsItem = id => {
@@ -24,7 +54,7 @@ export default class App extends Component {
      };
     
     render() {
-        let posts =  _.sortBy(api.getAll(), post => -post.upvotes);
+      /*  let posts =  _.sortBy(api.getAll(), post => -post.upvotes);
         return (
             <div className="container-fluid">
             <div className="row">
@@ -38,4 +68,22 @@ export default class App extends Component {
           </div>
         );
     }
+    */
+    const posts = _.sortBy(this.state.posts, post => post.upvotes);
+    return (
+      <div className="container">
+          <div className="row">
+              <div className="col-md-9 col-md-offset-1">
+                  <NewsList posts={posts} 
+                  upvoteHandler={this.incrementUpvote} />
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-md-9 col-md-offset-1">
+                  <Form handleAdd={ this.addNewsItem } />
+              </div>
+          </div>
+      </div>
+  );
+}
 }
